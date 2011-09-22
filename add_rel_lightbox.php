be081ea638a9be0de73e3d9cf9f7eebcc6060194
DefProc <contact@def-proc.co.uk>
2011-09-22 23:20:36 +0100

    Added support for lightbox on wordpress galleries *experimental*
    Replaces [gallery] shortcode with an almost identical copy from the
    WP trunk code (after 3.2.1).
    
    Adjustments find and preg_replace the rel=lightbox and title
    attributes into the link. I don't know of doing this without
    replacing the [gallery] shortcode. But it does mean that this has
    to be kept up to date with any changes to WP core.

--- add_rel_lightbox.php
+++ add_rel_lightbox.php
@@ -2,10 +2,21 @@
 /*
 Plugin Name: add_rel_lightbox
 Description: Add rel="lightbox[this_page]" to &lt;a&gt; wrapped image links in the content, and include captions for lightbox/slimbox
-Version: 0.1.1
+Version: 0.2-development
 Author: Patrick Fenner (Def-Proc.co.uk)
 Author URI: http://www.deferredprocrastination.co.uk/
 */
+
+/* Filter Hook */
+
+add_filter('the_content', 'add_rel_lightbox', 100);
+
+remove_shortcode('gallery');
+add_shortcode('gallery', 'add_rel_lighbox_gallery');
+
+/**
+ * Add-rel-lighbox
+ */
 
 function add_rel_lightbox($content)
 {
@@ -33,8 +44,153 @@ function add_rel_lightbox($content)
 	return $content;
 }
 
-/* Filter Hook */
+/**
+ * The Gallery shortcode.
+ *
+ * This implements the functionality of the Gallery Shortcode for displaying
+ * WordPress images on a post.
+ *
+ * @since 2.5.0
+ *
+ * @param array $attr Attributes attributed to the shortcode.
+ * @return string HTML content to display gallery.
+ */
 
-add_filter('the_content', 'add_rel_lightbox', 2);
+/**
+ * This is almost identical to the most recent gallery_shortcode in 
+ * wp-includes/media.php. Variations for add_rel_lighbox function are noted 
+ * inline.
+ */
+
+function add_rel_lighbox_gallery($attr) { 
+// ^ new function name to prevent clashing ^
+	global $post, $wp_locale;
+
+	static $instance = 0;
+	$instance++;
+
+	// Allow plugins/themes to override the default gallery template.
+	$output = apply_filters('post_gallery', '', $attr);
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr));
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "gallery-{$instance}";
+
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "
+		<style type='text/css'>
+			#{$selector} {
+				margin: auto;
+			}
+			#{$selector} .gallery-item {
+				float: {$float};
+				margin-top: 10px;
+				text-align: center;
+				width: {$itemwidth}%;
+			}
+			#{$selector} img {
+				border: 2px solid #cfcfcf;
+			}
+			#{$selector} .gallery-caption {
+				margin-left: 0;
+			}
+		</style>
+		<!-- see add_rel_lightbox_gallery() in wp-content/plugins/add-rel-lighbox/add_rel_lighbox.php -->";
+		// ^ adjusted css file reference to this file ^
+	$size_class = sanitize_html_class( $size );
+	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+		if ( isset($attr['link']) && 'file' == $attr['link'] ) {
+			// Then re-arrange the link to include the caption and rel="lightbox"
+			$pattern = "/title='(.*)'/"; // notice quote usage. <a… title=''><img… title=""></a>!
+			$slimbox_caption = esc_attr( $attachment->post_content );
+			$replace = 'rel="lightbox[gallery]" title="' . $slimbox_caption . '"';
+			$link = preg_replace($pattern, $replace, $link, 1);
+			// ^ replace only the first title="*" ^
+		}
+		
+		$output .= "<{$itemtag} class='gallery-item'>";
+		$output .= "
+			<{$icontag} class='gallery-icon'>
+				$link
+			</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='wp-caption-text gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+		if ( $columns > 0 && ++$i % $columns == 0 )
+			$output .= '<br style="clear: both" />';
+	}
+
+	$output .= "
+			<br style='clear: both;' />
+		</div>\n";
+
+	return $output;
+}
 
 ?>
